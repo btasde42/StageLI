08cb38519837a081d4c3bc37268c7e1b8d493a6d
Beyza Tasdelen <beyzatasdelen@MacBook-Air-de-Beyza.local>
2019-04-29 22:24:27 +0200

    Update dictionnary

--- Codes/bytestring_dict.py
+++ Codes/bytestring_dict.py
@@ -11,9 +11,10 @@ def bytestring_dict(symbol):
 	Returns:
 		a char representing utf-8 value
 	"""
-	Dict_byte={"\xc9\x9b":'ɛ',"\xc9\x99":'ə',"\xc9\x94\xcc\x83":'ɔ̃',"\xc3\xb8":'ø',"\xca\x83":'ʃ',"\xca\x81":'ʁ',"\xc9\x91\xcc\x83":"ɑ̃","\xca\x92":'ʒ',"\xc5\x93\xcc\x83":'œ̃',"\xc9\x94":'ɔ',"\xc5\x93":'œ',"\xc9\xa1":'g',"\xc9\x9b\xcc\x83":'ɛ̃',"\xcb\x90":'ː'}
+	Dict_byte={'\xc9\x9b':'ɛ','\xc9\x99':'ə','\xc9\x94\xcc\x83':'ɔ̃','\xc9\xb2':'ɲ','\xc3\xb8':'ø','\xca\x83':'ʃ','\xca\x81':'ʁ','\xc9\x91\xcc\x83':'ɑ̃','\xca\x92':'ʒ','\xc5\x93\xcc\x83':'œ̃','\xc9\x94':'ɔ','\xc5\x93':'œ','\xc9\xa1':'g','\xc9\x9b\xcc\x83':'ɛ̃','\xcb\x90':'ː'}
 	
-	for key,value in dictionnary:
+	for key,value in Dict_byte:
 		if symbol == key:
 			return value
-
+		else:
+			return symbol
